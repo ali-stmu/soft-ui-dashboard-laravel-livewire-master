01b8d6bdc6c8871e3441e0a4af9a78974e5c2c9f
Muhammad Ali <ali.mis@stmu.edu.pk>
2024-11-20 10:50:38 +0500

    status id migration

---
+++ database/migrations/2024_11_20_054558_add_status_id_to_oric_form_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusIdToOricFormTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('oric_form', function (Blueprint $table) {
+            $table->unsignedBigInteger('status_id')->nullable()->after('user_id'); // Adjust 'after' to place it correctly in your table
+            $table->foreign('status_id')->references('id')->on('oric_form_status')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('oric_form', function (Blueprint $table) {
+            $table->dropForeign(['status_id']);
+            $table->dropColumn('status_id');
+        });
+    }
+};
